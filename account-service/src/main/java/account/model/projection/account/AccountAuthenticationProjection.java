bcda98fa5658db1291f786a95f267647ddeee9aa
nsonanh <sonanhandynguyen@gmail.com>
2017-07-29 22:10:52 +0700

    [feature-projection-authentication-impl] Implement account projection for authentication

---
+++ account-service/src/main/java/account/model/projection/account/AccountAuthenticationProjection.java
@@ -0,0 +1,13 @@
+package account.model.projection.account;
+
+import account.model.Account;
+import org.springframework.data.rest.core.config.Projection;
+
+/**
+ * Created by nsonanh on 29/07/2017.
+ */
+@Projection(name = "authenticateAccount", types = Account.class)
+public interface AccountAuthenticationProjection {
+    String getUserName();
+    String getPass();
+}
