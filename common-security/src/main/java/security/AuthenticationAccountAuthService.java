d366752b300759a8af62f2f27befa9fa1b32c026
nsonanh <sonanhandynguyen@gmail.com>
2017-07-30 01:48:35 +0700

    [feature-token-impl] Implement token handling

--- common-security/src/main/java/security/AuthenticationAccountAuthService.java
+++ common-security/src/main/java/security/AuthenticationAccountAuthService.java
@@ -1,30 +1,45 @@
 package security;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.token.Token;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.client.RestTemplate;
 import security.model.AuthenticationAccount;
+
+import java.io.IOException;
 
 /**
  * Created by nsonanh on 29/07/2017.
  */
 public class AuthenticationAccountAuthService {
-    private static final String ACCOUNT_URI = "localhost:8081/account";
+    private static final String ACCOUNT_URI = "localhost:8081/accounts";
 
-    public AuthenticationAccount authenticateAccount(String userName, String pass) {
-        AuthenticationAccount account = getAuthenticationAccountFromAccountService(userName);
+    public ResponseEntity authenticateAccount(AuthenticationAccount request) {
+        AuthenticationAccount account = getAuthenticationAccountFromAccountService(request.getUserName());
 
         if (account == null) {
             throw new UsernameNotFoundException("Username not found");
         }
 
-        if (!Utils.match(pass, account.getPass())) {
+        if (!Utils.match(request.getPass(), account.getPass())) {
             throw new AuthenticationCredentialsNotFoundException("Authentication failed");
         }
 
-        // TODO generate & assign token
+        Token token = Utils.generateToken(account.getUserName());
+        return ResponseEntity.status(HttpStatus.OK).header("access-token", token.getKey())
+                .body(account.getUserName());
+    }
 
-        return account;
+    public Boolean authenticateToken(String tokenString) throws IOException {
+        if (tokenString == null) {
+            return false;
+        }
+        Token token = Utils.verifyToken(tokenString);
+        String userName = token.getExtendedInformation();
+
+        return userName != null && (System.currentTimeMillis() - token.getKeyCreationTime()) < Utils.DAY;
     }
 
     private AuthenticationAccount getAuthenticationAccountFromAccountService(String userName)
